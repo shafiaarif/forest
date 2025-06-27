2f4997054748b7f1de2b566037d5ee1cb27755a8
SitwatSamara <sitwatsamara@gmail.com>
2025-06-27 21:13:55 +0500

    Add my folder with content

---
+++ api/get_points.py
@@ -0,0 +1,8 @@
+from fastapi import HTTPException, Query as FastAPIQuery
+# from db import SupabaseConnection
+# from sql.query import GetUserByEmailAndRole, GetUserById, GetJobById, GetApplicationById, GetAllJobs, GetAllUsersWithDetails
+from .base import BaseRouter
+
+
+class GetRoutes(BaseRouter):
+    def __init__(self):
